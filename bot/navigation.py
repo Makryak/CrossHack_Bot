3288c2391a6e814b81ccf3fd7ca125a1cae819b8
Makar <130247888+Makryak@users.noreply.github.com>
2024-05-27 16:55:40 +0300

    Update navigation.py

--- bot/navigation.py
+++ bot/navigation.py
@@ -9,14 +9,14 @@ MainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 button_courses = KeyboardButton('/Courses')  # ПЕРЕДЕЛАТЬ
 button_enroll = KeyboardButton('/Enroll')  # ДОДЕЛАТЬ
 button_submit_homework = KeyboardButton('/submit_homework')
-MainMenu.add(button_courses, button_enroll)
+MainMenu.add(button_courses, button_enroll, button_submit_homework)
 
 # Админ меню
 AdminMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 button_add_course = KeyboardButton('/addcourse')  # 50/50 ПЕРЕДЕЛАТЬ
 # button_update_course = KeyboardButton('Update Courses') # СДЕЛАТЬ
 # button_modify_course_teacher = KeyboardButton('Modify My Courses') # СДЕЛАТЬ
-button_set_rules = KeyboardButton('/set_rules') # ГОТОВО (ВРОДЕ)
+button_set_rules = KeyboardButton('/setrules') # ГОТОВО (ВРОДЕ)
 button_set_appointment = KeyboardButton('/set_appointment')  # ДОДЕЛАТЬ
 button_view_homework = KeyboardButton('/view_homework')
 button_send_notification = KeyboardButton('/send_announcement')  # ГОТОВО
