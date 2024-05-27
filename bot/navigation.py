24f19903c7b62e561763d87c9f508fa82b5d67f5
Makar <130247888+Makryak@users.noreply.github.com>
2024-05-27 09:21:51 +0300

    Update navigation.py

--- bot/navigation.py
+++ bot/navigation.py
@@ -8,6 +8,7 @@ Resizer.add(Button123)
 MainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 button_courses = KeyboardButton('/Courses')  # ПЕРЕДЕЛАТЬ
 button_enroll = KeyboardButton('/Enroll')  # ДОДЕЛАТЬ
+button_submit_homework = KeyboardButton('/submit_homework')
 MainMenu.add(button_courses, button_enroll)
 
 # Админ меню
@@ -15,16 +16,16 @@ AdminMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 button_add_course = KeyboardButton('/addcourse')  # 50/50 ПЕРЕДЕЛАТЬ
 # button_update_course = KeyboardButton('Update Courses') # СДЕЛАТЬ
 # button_modify_course_teacher = KeyboardButton('Modify My Courses') # СДЕЛАТЬ
-# button_delete_user = KeyboardButton('Delete User') # ГОТОВО (ВРОДЕ)
-button_view_homework = KeyboardButton('/set_appointment')  # ДОДЕЛАТЬ
-button_send_notification = KeyboardButton('/send_announcement')  # ГОТОВО (ДОДЕЛАТЬ)
-button_change_rules = KeyboardButton('Change User Rules(НС)')  # СДЕЛАТЬ
-AdminMenu.add(button_add_course, button_view_homework, button_send_notification, button_change_rules)
+button_set_rules = KeyboardButton('/set_rules') # ГОТОВО (ВРОДЕ)
+button_set_appointment = KeyboardButton('/set_appointment')  # ДОДЕЛАТЬ
+button_view_homework = KeyboardButton('/view_homework')
+button_send_notification = KeyboardButton('/send_announcement')  # ГОТОВО
+AdminMenu.add(button_add_course, button_set_appointment, button_view_homework, button_set_rules, button_send_notification)
 
 # Меню преподавателя
 TeacherMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 button_add_course_teacher = KeyboardButton('/addcourse')  # 50/50 ПЕРЕДЕЛАТЬ
 button_modify_course_teacher = KeyboardButton('Modify My Courses(НС)')  # СДЕЛАТЬ
-button_view_homework = KeyboardButton('View Homework(НС)')  # ДОДЕЛАТЬ
+button_view_homework = KeyboardButton('/view_homework')  # ДОДЕЛАТЬ
 button_send_announcement = KeyboardButton('/send_announcement')  # ГОТОВО
 TeacherMenu.add(button_add_course_teacher, button_modify_course_teacher, button_view_homework, button_send_announcement)
